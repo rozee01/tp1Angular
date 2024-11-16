63522aeee9ada43c4671110d5d0a3074259463da
Arij Thabet <arijthabet01@gmail.com>
2024-11-16 11:01:19 +0100

    correct ttc

--- src/app/ttc/ttc.component.ts
+++ src/app/ttc/ttc.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-ttc',
@@ -6,22 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './ttc.component.css'
 })
 export class TTCComponent {
-  quantity: number = 1;
-  unitPrice: number = 0;
-  tva: number = 18;
-  get priceHT(): number {
-    return this.quantity * this.unitPrice;
-  }
-  get coef(): number {
-    if (this.quantity > 15) {
+  quantity=signal(1);
+  unitPrice=signal(0) ;
+  tva=signal(18);
+
+  unitPricettc: Signal<number> = computed(()=>{
+    return this.unitPrice()*(1+ this.tva()/100);
+  });
+  totalPriceTTC: Signal<number> = computed(() => {
+    let total = this.unitPricettc() * this.quantity();
+    return total*this.coef();
+  });
+  
+  coef: Signal<number>= computed(()=> {
+    if (this.quantity() > 15) {
       return 0.70; // 30% remise
-    } else if (this.quantity > 10) {
+    } else if (this.quantity() > 10) {
       return 0.80; // 20% de remise
     }
     return 1; // Pas de remise
-  }
-  get priceTTC(): number {
-    const priceAfterDiscount = this.priceHT * this.coef;
-    return priceAfterDiscount * (1 + this.tva / 100); // Appliquer la TVA
-  }
+  });
 }
